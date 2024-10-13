356e44a1df326bce83d6d9d65fb2b06159e8dfe9
truien <trongtruyen04@gmail.com>
2024-10-13 20:10:30 +0700

    chiu

--- obj/Debug/net8.0/TopCV.AssemblyInfo.cs
+++ obj/Debug/net8.0/TopCV.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TopCV")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+26beb243de4017e456155d03e397faed08d598cd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+21aac8283f7d09529494c10324b7c31fc70e180a")]
 [assembly: System.Reflection.AssemblyProductAttribute("TopCV")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TopCV")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
