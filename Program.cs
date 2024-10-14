bb6c3cea2f397039d56dcfe51f861b7a1777a808
truien <trongtruyen04@gmail.com>
2024-10-15 02:12:54 +0700

    Initial commit for viewreact project

--- Program.cs
+++ Program.cs
@@ -1,21 +1,10 @@
-using Microsoft.EntityFrameworkCore; 
-using TopCV.data;
-using Pomelo.EntityFrameworkCore.MySql;
-
 var builder = WebApplication.CreateBuilder(args);
 
-
-var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
-
-
-builder.Services.AddDbContext<ApplicationDbContext>(options =>
-    options.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString)));
-
-builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
+
 
 if (app.Environment.IsDevelopment())
 {
@@ -25,8 +14,7 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-app.UseAuthorization();
 
-app.MapControllers();
 
 app.Run();
+
--- obj/Debug/net8.0/TopCV.AssemblyInfo.cs
+++ obj/Debug/net8.0/TopCV.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TopCV")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+21aac8283f7d09529494c10324b7c31fc70e180a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+356e44a1df326bce83d6d9d65fb2b06159e8dfe9")]
 [assembly: System.Reflection.AssemblyProductAttribute("TopCV")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TopCV")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
